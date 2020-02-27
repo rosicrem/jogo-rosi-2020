2bb10e1cd89ddf7b2a78b077085d8cedc450b065
Rosilene <rosi.vinhal@gmail.com>
2020-02-27 11:09:33 -0300

    NavMeshAgent basic move

---
+++ Assets/Scripts/Characters/PlayerControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class PlayerControl : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Camera cam;
+    public LayerMask walkablelayer;
+    void Start()
+    {
+        agent = GetComponent <NavMeshAgent>();
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, walkablelayer))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
