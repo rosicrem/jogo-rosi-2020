0a842883557fa8abc2bfabce93d3d826f900f418
Rosilene <rosi.vinhal@gmail.com>
2020-03-05 10:13:31 -0300

    TouchableGameObject, Stats

---
+++ Assets/Scripts/NavGame/Core/TouchableGameObjetc.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NavGame.Core
+{
+
+    public class TouchableGameObjetc : MonoBehaviour
+
+    {
+        public float contactRadius = 0.5f;
+        public bool IsInTouch(TouchableGameObjetc other)
+        {
+            float distance = Vector3.Distance(transform.position, other.transform.position);
+            return distance < contactRadius + other.contactRadius;
+        }
+
+        protected virtual void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(transform.position, contactRadius);
+        }
+    }
+}
