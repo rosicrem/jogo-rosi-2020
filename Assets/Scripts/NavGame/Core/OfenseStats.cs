f02433c154ae44cbea6a5c441e890cb4a25c037d
Rosilene <rosi.vinhal@gmail.com>
2020-03-26 12:07:59 -0300

    Ofense e Defense Stats

---
+++ Assets/Scripts/NavGame/Core/OfenseStats.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NavGame.Core
+{
+    [Serializable]
+
+    public class OfenseStats
+    {
+        
+        public int damage = 10;
+        public float attackSpeed = 1f;
+    }
+}
+
