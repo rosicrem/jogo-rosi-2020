79c8c56ede4f298c73bd1df4a739e1dfa81d6681
Rosilene <rosi.vinhal@gmail.com>
2020-04-26 22:18:16 -0300

    Projectile Spinner

---
+++ Assets/Scripts/NavGame/Misc/Spinner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NavGame.Misc
+{
+
+    public class Spinner : MonoBehaviour
+    {
+
+        public Vector3 eulersPerSecond;
+
+        void Update()
+        {
+            transform.Rotate(eulersPerSecond * Time.deltaTime);
+        }
+    }
+}
