dc16a0aa6536f2d2dad12e00177abc1797fd98b2
Rosilene <rosi.vinhal@gmail.com>
2020-05-13 20:35:35 -0300

    Wave Update event

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -10,6 +10,7 @@ public class UIManager : MonoBehaviour
     public Text errorText;
     public float errorTime = 1.5f;
     public Text coinText;
+    public Text waveCountText;
     public GameObject[] cooldownObjects;
     public Text[] actionCosts;
     Image[] cooldownImages;
@@ -21,8 +22,9 @@ public class UIManager : MonoBehaviour
         LevelManager.instance.onActionCooldownUpdateEvent += OnActionCooldownUpdate;
         LevelManager.instance.onResourceUpdate += OnResourceUpdate;  
         LevelManager.instance.onReportableError += OnReportableError;
-    }
+        LevelManager.instance.onWaveUpdate += OnWaveUpdate;
     
+    }
     void Start()
     {
         InitializeUI();
@@ -68,6 +70,11 @@ public class UIManager : MonoBehaviour
         errorText.text = message;
         errorPanel.SetActive(true);
         StartCoroutine(TurnOffError());
+    }
+
+    void OnWaveUpdate(int totalWaves, int currentWave)
+    {
+        waveCountText.text = currentWave + " / " + totalWaves;
     }
 
     IEnumerator TurnOffError()
